5580e80f49adffc6497317fbfc5655f3c89665fa
amretasrerengarajan <amretasrerengarajan@localhost>
2022-09-28 19:24:28 -0400

    This is LAB 1 Application

---
+++ Lab_1/src/model/Product.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author amretasrerengarajan
+ */
+public class Product {
+    
+    private String name;
+    private String descr;
+    private String availNum;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+    
+    
+}
